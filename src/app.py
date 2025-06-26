2a0cda2990ee76103242e6522b9a844d6c137b04
Renan <Renan de Moraes>
2025-06-26 19:57:03 -0300

    alterando o port no app.py para que o render possa ler

--- src/app.py
+++ src/app.py
@@ -248,4 +248,5 @@ def register_routes(app):
 if __name__ == '__main__':
     # Criar e executar a aplicação
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
